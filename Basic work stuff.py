c5043bd2fdcacfdede06b21e1e8d441cd7fef4f7
Pranav Aseri <aseripranav@gmail.com>
2026-05-21 11:49:13 +0530

    Add files via upload

---
+++ Basic work stuff.py
@@ -0,0 +1,29 @@
+def add():
+    a = 6
+    b = 7
+    print (a + b)
+add()
+
+def greet(): # create a function called greet 
+    print("Hello")
+greet() #call the function
+
+
+def add(a,b): # parameter a and b 
+    print(a + b)
+add (5,10) # call the function pass the value  of a and b 
+
+def greet(name):     #parameter name
+    print("hello " + name)
+greet("Pranav")      #call the function and pass the value of name 
+
+def add(a,b):# its a function for return 
+    return a + b
+print (add(4,6)) # call te function and print the result
+
+def add(a, b):
+    return a + b # return the sum of a and b
+
+result = add(5, 3)
+
+print(result) # print the result of the addition
